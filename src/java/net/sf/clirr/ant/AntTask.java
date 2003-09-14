c3833368af84a07249ed94037a176e78fab994b1
lkuehne <lkuehne>
2003-09-14 10:54:37 +0000

    fixed file header

--- src/java/net/sf/clirr/ant/AntTask.java
+++ src/java/net/sf/clirr/ant/AntTask.java
@@ -1,5 +1,5 @@
-///////////////////////////////////////////////////////////////////////////////
-// clirr: compares two versions of a java library for binary compatibility
+//////////////////////////////////////////////////////////////////////////////
+// Clirr: compares two versions of a java library for binary compatibility
 // Copyright (C) 2003  Lars Kühne
 //
 // This library is free software; you can redistribute it and/or
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 package net.sf.clirr.ant;
 
