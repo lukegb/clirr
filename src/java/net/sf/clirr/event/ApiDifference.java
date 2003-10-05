699d74972afd90b00ce79c42b9d8f1feffe9cd7c
lkuehne <lkuehne>
2003-10-05 17:56:31 +0000

    override equals so unit tests can compare expected and actual ApiDiffs

--- src/java/net/sf/clirr/event/ApiDifference.java
+++ src/java/net/sf/clirr/event/ApiDifference.java
@@ -34,15 +34,29 @@ public final class ApiDifference
 
     private Severity severity;
 
-    private boolean expected;
+    //private boolean expected;
 
+    /**
+     * Create a new API differnce representation.
+     *
+     * @param report a human readable string describing the change that was made.
+     * @param severity the severity in terms of binary API compatibility.
+     */
     public ApiDifference(String report, Severity severity /*, boolean expected*/)
     {
         this.report = report;
         this.severity = severity;
-        this.expected = expected;
+//        this.expected = expected;
     }
 
+    /**
+     * The Severity of the API difference. ERROR means that clients will
+     * definately break, WARNING means that clients may break, depending
+     * on how they use the library. See the eclipse paper for further
+     * explanation.
+     *
+     * @return the severity of the API difference.
+     */
     public Severity getSeverity()
     {
         return severity;
@@ -53,13 +67,51 @@ public final class ApiDifference
         return report;
     }
 
+    /*
     public boolean isExpected()
     {
         return expected;
     }
+    */
 
+    /** {@inheritDoc} */
     public String toString()
     {
-        return report;
+        return report + " (" + severity + ")";
+    }
+
+    /** {@inheritDoc} */
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+
+        if (!(o instanceof ApiDifference))
+        {
+            return false;
+        }
+
+        final ApiDifference apiDifference = (ApiDifference) o;
+
+        if (report != null ? !report.equals(apiDifference.report) : apiDifference.report != null)
+        {
+            return false;
+        }
+        if (severity != null ? !severity.equals(apiDifference.severity) : apiDifference.severity != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode()
+    {
+        int result;
+        result = (report != null ? report.hashCode() : 0);
+        result = 29 * result + (severity != null ? severity.hashCode() : 0);
+        return result;
     }
 }
