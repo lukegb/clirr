b39ae90f3bf8c448dafc2a72794814c4de48b3ae
lkuehne <lkuehne>
2004-06-20 16:08:21 +0000

    added test data for recent methodcheck changes

---
+++ src/testinput/testlib-v2/testlib/AbstractImpl.java
@@ -0,0 +1,6 @@
+package testlib;
+
+public abstract class AbstractImpl implements BaseInterface
+{
+    // public abstract void method(); not available here directly
+}
---
+++ src/testinput/testlib-v2/testlib/BaseInterface.java
@@ -0,0 +1,6 @@
+package testlib;
+
+public interface BaseInterface
+{
+    void method();
+}
