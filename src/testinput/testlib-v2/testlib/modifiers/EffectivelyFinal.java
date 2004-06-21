1b6411d854745dde615d3df43dbe2738e14e2673
s_kitching <s_kitching>
2004-06-21 07:17:00 +0000

    tests for ClassModifiersCheck

---
+++ src/testinput/testlib-v2/testlib/modifiers/EffectivelyFinal.java
@@ -0,0 +1,28 @@
+package testlib.modifiers;
+
+/**
+ * Because this class has a package-scope constructor, no subclass of this
+ * class can ever be created. And because of that, it is not a problem if
+ * this class is declared "final" in a later version.
+ * <p>
+ * Classes with only private constructors are commonly used to implement
+ * an "enumerated type" in java, as is done here.
+ */
+
+public final class EffectivelyFinal
+{
+    int val;
+    
+    public static final EffectivelyFinal ZERO = new EffectivelyFinal(0);
+    public static final EffectivelyFinal ONE = new EffectivelyFinal(1);
+    
+    private EffectivelyFinal(int i) 
+    {
+        val = i;
+    }
+    
+    public int getValue() 
+    {
+        return val;
+    }
+}
