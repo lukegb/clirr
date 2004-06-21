da339abc35ad3f829388dc951c8c7a2c957ed245
s_kitching <s_kitching>
2004-06-21 06:59:36 +0000

    Changes due to FieldSetCheck change; a field changed from private to public is now reported as "accessability changed" rather than "field added".

--- src/test/net/sf/clirr/checks/FieldSetCheckTest.java
+++ src/test/net/sf/clirr/checks/FieldSetCheckTest.java
@@ -18,7 +18,7 @@ public class FieldSetCheckTest extends AbstractCheckTestCase
             new ApiDifference("Field stat7 has been removed in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "stat7"),
             new ApiDifference("Accessibility of field fin2 has been weakened from public to protected in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "fin2"),
             new ApiDifference("Accessibility of field stat4 has been weakened from public to protected in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "stat4"),
-            new ApiDifference("Added public field priv2 in testlib.MembersChange", Severity.INFO, "testlib.MembersChange", null, "priv2"),
+            new ApiDifference("Accessability of field priv2 has been increased from private to public in testlib.MembersChange", Severity.INFO, "testlib.MembersChange", null, "priv2"),
             new ApiDifference("Accessibility of field stat5 has been weakened from public to private in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "stat5"),
             new ApiDifference("Field stat2 is now final in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "stat2"),
             new ApiDifference("Field pub3 is now final in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "pub3"),
@@ -30,8 +30,8 @@ public class FieldSetCheckTest extends AbstractCheckTestCase
             new ApiDifference("Field pub2 is now static in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "pub2"),
             new ApiDifference("Changed type of field obj1 from java.lang.Object to java.lang.String in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "obj1"),
             new ApiDifference("Changed type of field obj2 from java.lang.Boolean to java.lang.String in testlib.MembersChange", Severity.ERROR, "testlib.MembersChange", null, "obj2"),
-            new ApiDifference("Value of compile time constant fin5 has been changed in testlib.MembersChange", Severity.WARNING, "testlib.MembersChange", null, "fin5"),
             new ApiDifference("Value of fin6 is no longer a compile time constant in testlib.MembersChange", Severity.WARNING, "testlib.MembersChange", null, "fin6"),
+            new ApiDifference("Value of compile time constant fin5 has been changed in testlib.MembersChange", Severity.WARNING, "testlib.MembersChange", null, "fin5"),
         };
         verify(expected);
     }
