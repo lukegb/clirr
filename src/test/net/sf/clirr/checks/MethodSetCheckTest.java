ea6c3ce531863ec15716f7fd5389169dfb26ec69
s_kitching <s_kitching>
2004-06-29 06:56:46 +0000

    Removed the "..in {classname}" from the end of each generated message.

--- src/test/net/sf/clirr/checks/MethodSetCheckTest.java
+++ src/test/net/sf/clirr/checks/MethodSetCheckTest.java
@@ -17,50 +17,50 @@ public class MethodSetCheckTest extends AbstractCheckTestCase
         ApiDifference[] expected = new ApiDifference[] {
 
             // method addition and removal
-            new ApiDifference("Method 'public void removedMethod(java.lang.String)' has been removed in testlib.MethodsChange",
+            new ApiDifference("Method 'public void removedMethod(java.lang.String)' has been removed",
                     Severity.ERROR, "testlib.MethodsChange", "public void removedMethod(java.lang.String)", null),
-            new ApiDifference("Accessability of method 'public int getPriv2()' has been decreased from public to private in testlib.MethodsChange",
+            new ApiDifference("Accessability of method 'public int getPriv2()' has been decreased from public to private",
                     Severity.ERROR, "testlib.MethodsChange", "public int getPriv2()", null),
-            new ApiDifference("Method 'protected MethodsChange(int, boolean)' has been added in testlib.MethodsChange",
+            new ApiDifference("Method 'protected MethodsChange(int, boolean)' has been added",
                     Severity.INFO, "testlib.MethodsChange", "protected MethodsChange(int, boolean)", null),
-            new ApiDifference("Method 'public java.lang.Long getPrivSquare()' has been added in testlib.MethodsChange",
+            new ApiDifference("Method 'public java.lang.Long getPrivSquare()' has been added",
                     Severity.INFO, "testlib.MethodsChange", "public java.lang.Long getPrivSquare()", null),
 
-            new ApiDifference("Method 'public void moveToSuper()' has been added in testlib.ComplexMethodMoveBase",
+            new ApiDifference("Method 'public void moveToSuper()' has been added",
                     Severity.INFO, "testlib.ComplexMethodMoveBase", "public void moveToSuper()", null),
-            new ApiDifference("Method 'public void moveToSuper()' is now implemented in superclass testlib.ComplexMethodMoveBase in testlib.ComplexMethodMoveSub",
+            new ApiDifference("Method 'public void moveToSuper()' is now implemented in superclass testlib.ComplexMethodMoveBase",
                     Severity.INFO, "testlib.ComplexMethodMoveSub", "public void moveToSuper()", null),
 
-            new ApiDifference("Abstract method 'public void method()' is now specified by implemented interface testlib.BaseInterface in testlib.AbstractImpl",
+            new ApiDifference("Abstract method 'public void method()' is now specified by implemented interface testlib.BaseInterface",
                     Severity.INFO, "testlib.AbstractImpl", "public void method()", null),
 
             // Constructor changes
-            new ApiDifference("Parameter 1 of 'protected MethodsChange(int)' has changed its type to java.lang.Integer in testlib.MethodsChange",
+            new ApiDifference("Parameter 1 of 'protected MethodsChange(int)' has changed its type to java.lang.Integer",
                     Severity.ERROR, "testlib.MethodsChange", "protected MethodsChange(int)", null),
 
             // return type changes
-            new ApiDifference("Return type of method 'public java.lang.Number getPrivAsNumber()' has been changed to java.lang.Integer in testlib.MethodsChange",
+            new ApiDifference("Return type of method 'public java.lang.Number getPrivAsNumber()' has been changed to java.lang.Integer",
                     Severity.ERROR, "testlib.MethodsChange", "public java.lang.Number getPrivAsNumber()", null),
             // TODO: INFO if method is final
-            new ApiDifference("Return type of method 'public java.lang.Integer getPrivAsInteger()' has been changed to java.lang.Number in testlib.MethodsChange",
+            new ApiDifference("Return type of method 'public java.lang.Integer getPrivAsInteger()' has been changed to java.lang.Number",
                     Severity.ERROR, "testlib.MethodsChange", "public java.lang.Integer getPrivAsInteger()", null),
 
             // parameter list changes
             // Note: This is the current behaviour, not necessarily the spec of the desired behaviour
             // TODO: need to check assignability of types (and check if method or class is final?)
-            new ApiDifference("In method 'public void printPriv()' the number of arguments has changed in testlib.MethodsChange",
+            new ApiDifference("In method 'public void printPriv()' the number of arguments has changed",
                     Severity.ERROR, "testlib.MethodsChange", "public void printPriv()", null),
-            new ApiDifference("Parameter 1 of 'public void strengthenParamType(java.lang.Object)' has changed its type to java.lang.String in testlib.MethodsChange",
+            new ApiDifference("Parameter 1 of 'public void strengthenParamType(java.lang.Object)' has changed its type to java.lang.String",
                     Severity.ERROR, "testlib.MethodsChange", "public void strengthenParamType(java.lang.Object)", null),
-            new ApiDifference("Parameter 1 of 'public void weakenParamType(java.lang.String)' has changed its type to java.lang.Object in testlib.MethodsChange",
+            new ApiDifference("Parameter 1 of 'public void weakenParamType(java.lang.String)' has changed its type to java.lang.Object",
                     Severity.ERROR, "testlib.MethodsChange", "public void weakenParamType(java.lang.String)", null),
-            new ApiDifference("Parameter 1 of 'public void changeParamType(java.lang.String)' has changed its type to java.lang.Integer in testlib.MethodsChange",
+            new ApiDifference("Parameter 1 of 'public void changeParamType(java.lang.String)' has changed its type to java.lang.Integer",
                     Severity.ERROR, "testlib.MethodsChange", "public void changeParamType(java.lang.String)", null),
 
             // deprecation changes
-            new ApiDifference("Method 'public void becomesDeprecated()' has been deprecated in testlib.MethodsChange",
+            new ApiDifference("Method 'public void becomesDeprecated()' has been deprecated",
                     Severity.INFO, "testlib.MethodsChange", "public void becomesDeprecated()", null),
-            new ApiDifference("Method 'public void becomesUndeprecated()' is no longer deprecated in testlib.MethodsChange",
+            new ApiDifference("Method 'public void becomesUndeprecated()' is no longer deprecated",
                     Severity.INFO, "testlib.MethodsChange", "public void becomesUndeprecated()", null),
 
             // declared exceptions
